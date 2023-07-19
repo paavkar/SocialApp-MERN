75067bc4ee5beff7afb2058c88c102e3d5e531f0
paavkar <paavo_karppinen@outlook.com>
2023-07-19 16:13:00 +0300

    Finished project. Added friendlist and advert widgets, some changes to the project from other people

--- Client/src/components/Friend.jsx
+++ Client/src/components/Friend.jsx
@@ -21,6 +21,7 @@ const Friend = ({ friendId, name, subtitle, userPictureData }) => {
   const medium = palette.neutral.medium;
 
   const isFriend = friends.find((friend) => friend._id === friendId);
+  const isSelf = friendId === _id;
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -65,16 +66,18 @@ const Friend = ({ friendId, name, subtitle, userPictureData }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {!isSelf && (
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>
+      )}
     </FlexBetween>
   );
 };
